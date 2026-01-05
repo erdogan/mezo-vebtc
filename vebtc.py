64c8ca1b4ce393c4ec4c34b0bc12393205b39892
Engin Erdogan <engin@thesis.co>
2026-01-04 23:07:35 -0500

    Add cache-busting headers to prevent browser caching issues
    - Added meta tags to force browser to reload latest HTML
    - Ensures date picker defaults are always visible
    - Fixes issue where end date appeared blank due to cached version

--- vebtc.py
+++ vebtc.py
@@ -304,6 +304,10 @@ def generate_dashboard(locks: List[Dict[str, Any]], votes: List[Dict[str, Any]],
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="Expires" content="0">
     <title>veBTC Locks & Votes</title>
     <script src="https://cdn.plot.ly/plotly-2.27.0.min.js"></script>
     <style>
